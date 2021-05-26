b2e3be360f78dcd4615208cdda7d29fc5e81756a
a20jasry <81618961+a20jasry@users.noreply.github.com>
2021-05-26 22:34:17 +0200

    /.idea/* *.zip added

--- app/src/main/java/com/example/assignment5project/MainActivity.java
+++ app/src/main/java/com/example/assignment5project/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.assignment5project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Noodles> item;
+    private ArrayAdapter<Noodles> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
